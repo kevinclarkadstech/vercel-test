28a99af04ebfde3611bea67029f757dc311c062e
Kevin Clark <kevin.clark.adstech@gmail.com>
2020-04-22 13:28:22 -0700

    Changed name

--- api/auth/login.ts
+++ api/auth/login.ts
@@ -2,5 +2,5 @@
 import { NowRequest, NowResponse } from '@now/node'
 
 export default (req: NowRequest, res: NowResponse) => {
-  res.json({ name: 'John', email: 'john@example.com' })
+  res.json({ name: 'Joe', email: 'joe@example.com' })
 }
