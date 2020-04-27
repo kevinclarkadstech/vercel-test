1a5f536beac7b42c817a88531736dd21f324ed08
Kevin Clark <kevin.clark.adstech@gmail.com>
2020-04-27 16:43:38 -0700

    Added an origin

--- api/auth/login.ts
+++ api/auth/login.ts
@@ -2,6 +2,6 @@
 import { NowRequest, NowResponse } from '@now/node'
 
 export default (req: NowRequest, res: NowResponse) => {
-  res.setHeader('Access-Control-Allow-Origin', '');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3333');
   res.json({ name: 'Fred', email: 'fred@example.com' })
 }
