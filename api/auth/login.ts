888824084d1f0b94e3912145e2813e5afdc7cacf
Kevin Clark <kevin.clark.adstech@gmail.com>
2020-04-27 16:53:21 -0700

    Chaanged method to put

--- api/auth/login.ts
+++ api/auth/login.ts
@@ -4,6 +4,9 @@ import { NowRequest, NowResponse } from '@now/node'
 export default (req: NowRequest, res: NowResponse) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3333');
   res.setHeader('Access-Control-Allow-Headers', 'Accept, Accept-Language, Content-Language, Content-Type');
-  res.setHeader('Access-Control-Allow-Methods', 'POST');
+  res.setHeader('Access-Control-Allow-Methods', 'PUT, OPTIONS');
+  if (req.method === 'OPTIONS') {
+    res.status(200).send({});
+  }
   res.json({ name: 'Fred', email: 'fred@example.com' })
 }
