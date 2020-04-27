4c9c7a3da0b540f191a8494c778722ee9e49b72e
Kevin Clark <kevin.clark.adstech@gmail.com>
2020-04-27 16:35:27 -0700

    pushing update

--- api/auth/login.ts
+++ api/auth/login.ts
@@ -2,5 +2,6 @@
 import { NowRequest, NowResponse } from '@now/node'
 
 export default (req: NowRequest, res: NowResponse) => {
+  res.setHeader('Access-Control-Allow-Origin', '');
   res.json({ name: 'Fred', email: 'fred@example.com' })
 }
