c0d6f8bec6fc47bce81d8b138c2fbda4f2bbe053
Kevin Clark <kevin.clark.adstech@gmail.com>
2020-04-27 16:50:37 -0700

    Added other headers

--- api/auth/login.ts
+++ api/auth/login.ts
@@ -3,5 +3,7 @@ import { NowRequest, NowResponse } from '@now/node'
 
 export default (req: NowRequest, res: NowResponse) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3333');
+  res.setHeader('Access-Control-Allow-Headers', 'Accept, Accept-Language, Content-Language, Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
   res.json({ name: 'Fred', email: 'fred@example.com' })
 }
