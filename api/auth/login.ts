b152dcbe98e9827fed86290fc1a28aa1592ec37f
Kevin Clark <kevin.clark.adstech@gmail.com>
2020-04-27 16:55:52 -0700

    Tried taking out options check

--- api/auth/login.ts
+++ api/auth/login.ts
@@ -5,8 +5,8 @@ export default (req: NowRequest, res: NowResponse) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3333');
   res.setHeader('Access-Control-Allow-Headers', 'Accept, Accept-Language, Content-Language, Content-Type');
   res.setHeader('Access-Control-Allow-Methods', 'PUT, OPTIONS');
-  if (req.method === 'OPTIONS') {
-    res.status(200).send({});
-  }
+  // if (req.method === 'OPTIONS') {
+  //   res.status(200).send({});
+  // }
   res.json({ name: 'Fred', email: 'fred@example.com' })
 }
