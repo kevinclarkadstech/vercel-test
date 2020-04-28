bd4a863f6f8ae64623076dc5a1e7c423fa757017
Kevin Clark <kevin.clark.adstech@gmail.com>
2020-04-27 17:23:02 -0700

    added header for auth token

--- api/auth/login.ts
+++ api/auth/login.ts
@@ -3,7 +3,7 @@ import { NowRequest, NowResponse } from '@now/node'
 
 export default (req: NowRequest, res: NowResponse) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3333');
-  res.setHeader('Access-Control-Allow-Headers', 'Accept, Accept-Language, Content-Language, Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Accept, Accept-Language, Content-Language, Content-Type, authToken');
   res.setHeader('Access-Control-Allow-Methods', 'PUT, OPTIONS');
   // if (req.method === 'OPTIONS') {
   //   res.status(200).send({});
